2b874ed514886be8ea25be3589b533d72d2d7b5e
Kiril Cvetanoski <kiril.cvetanoski@students.finki.ukim.mk>
2025-05-18 22:00:24 +0200

    Added Dark/White mode.

--- AiCademy/AiCademy.Web/wwwroot/js/site.js
+++ AiCademy/AiCademy.Web/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const applyTheme = (theme) => {
+    document.body.classList.remove('light-mode', 'dark-mode');
+    document.body.classList.add(`${theme}-mode`);
+};
+
+const savedTheme = localStorage.getItem('theme') || 'light';
+applyTheme(savedTheme);
+
+document.getElementById('themeToggleBtn').addEventListener('click', () => {
+    const newTheme = document.body.classList.contains('light-mode') ? 'dark' : 'light';
+    localStorage.setItem('theme', newTheme);
+    applyTheme(newTheme);
+});
